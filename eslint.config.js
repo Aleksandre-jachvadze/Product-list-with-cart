127fb4a781d6a835bcb50ef097e5b45495825b44
aleksandre jachvadze <155845898+Aleksandre-jachvadze@users.noreply.github.com>
2025-02-10 23:18:21 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,20 @@
+import tseslint from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: ['plugin:@typescript-eslint/recommended'],
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  }
+)
 
